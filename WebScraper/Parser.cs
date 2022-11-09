53a2dfc2c231df23550033e30773c3160d263109
Benek07 <wojciechbednarz96@gmail.com>
2022-11-09 20:10:31 +0100

    Project files

---
+++ WebScraper/Parser.cs
@@ -0,0 +1,247 @@
+﻿//#define SAVE_HTML
+#define USE_HTML_FILE
+
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebScraper
+{
+    class Parser
+    {
+        string projPath = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+
+        public List<Company> GetCompanies(string url, string fileName = null)
+        {
+            var companies = ParseCompanies("https://audio-database.com", "links");
+            foreach (var company in companies)
+            {
+                ParseCategory(company, true);
+                //ParseCategory("https://audio-database.com/MARANTZ", "Marantz");
+            }
+            return companies;
+        }
+
+        public List<Company> ParseCompanies(string url, string fileName = null)
+        {
+#if USE_HTML_FILE
+            string response = File.ReadAllText(Path.Combine(projPath, "test", "audio-database.html")); //for tests
+#else
+            string response = CallUrl(url).Result;
+#endif
+            var decodedhtml = HttpUtility.HtmlDecode(response);
+            var companyList = ParseHtmlCompanies(decodedhtml, url);
+
+#if SAVE_HTML
+            File.WriteAllText(Path.Combine(projPath, "test", "audio-database" + ".html"), decodedhtml);
+#endif
+            if (fileName != null)
+            {
+                WriteToCsv(companyList, fileName);
+            }
+            return companyList;
+        }
+
+        private List<Company> ParseHtmlCompanies(string html, string baseLink)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var links = htmlDoc.DocumentNode.SelectNodes("//li/a");
+
+            List<Company> companies = new List<Company>();
+            //Dictionary<string, string> companyLink = new Dictionary<string, string>();
+
+            foreach (var link in links)
+            {
+                if (link.ParentNode.FirstChild.Attributes.Count > 0)
+                {
+                    Company company = new Company
+                    {
+                        Name = link.InnerText,
+                        Link = baseLink + '/' + link.ParentNode.FirstChild.Attributes[0].Value,
+                    };
+                    companies.Add(company);
+                }
+                //companyLink.Add(link.InnerText, baseLink + '/' + link.ParentNode.FirstChild.Attributes[0].Value);
+            }
+
+            return companies;
+        }
+
+        private void WriteToCsv(List<Company> companies, string fileName)
+        {
+            String csv = String.Join(
+                Environment.NewLine,
+                companies.Select(d => $"{d.Name};{d.Link};")
+            );
+
+            File.WriteAllText(Path.Combine(projPath, "data", fileName + ".csv"), csv);
+        }
+
+        public void ParseCategory(string url, string fileName)
+        {
+            //string response = CallUrl(url).Result;
+            string response = File.ReadAllText(Path.Combine(projPath, "test", "MARANTZ.html")); //for tests
+            var decodedhtml = HttpUtility.HtmlDecode(response);
+            var linkList = ParseHtmlCategory(decodedhtml, url);
+
+            //WriteToCsv(linkList, fileName);
+        }
+
+        public void ParseCategory(Company company, bool saveToFile, string fileName = null)
+        {
+            string response = "";
+#if USE_HTML_FILE
+            string pathFile = Path.Combine(projPath, "test", company.Name + ".html");
+            if (File.Exists(pathFile))
+            {
+                response = File.ReadAllText(pathFile);
+            }
+#else
+            response = CallUrl(url).Result;
+#endif
+            if (!String.IsNullOrEmpty(response))
+            {
+                var decodedhtml = HttpUtility.HtmlDecode(response);
+                var categories = ParseHtmlCategory(decodedhtml, company);
+
+#if SAVE_HTML
+                File.WriteAllText(Path.Combine(projPath, "test", company.Name + ".html"), decodedhtml);
+#endif
+                if (saveToFile)
+                {
+                    foreach (Category category in categories)
+                    {
+                        WriteToCsv(category, fileName);
+                    }
+                }
+            }
+        }
+
+        private List<Category> ParseHtmlCategory(string html, Company company)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var node = htmlDoc.DocumentNode.SelectNodes("//li/a");
+
+            Regex r = new Regex(@"\w.*\w", RegexOptions.Multiline);
+
+            List<Category> categories = company.Categories;
+            if (node != null)
+            {
+                foreach (var link in node)
+                {
+                    Category category = categories.LastOrDefault();
+                    string catName = link.ParentNode.ParentNode.PreviousSibling.PreviousSibling.InnerText;
+
+                    if (catName != category?.Name)
+                    {
+                        category = new Category(catName);
+                        categories.Add(category);
+                    }
+                    if (link.Attributes.Count > 0)
+                    {
+                        string[] subCategories = r.Matches(link.InnerText)
+                                                .Cast<Match>()
+                                                .Select(m => m.Value)
+                                                .ToArray();
+
+                        if (subCategories != null)
+                        {
+                            string baseLink = company.Link.Substring(0, company.Link.Length - 10);
+                            foreach (string subCategory in subCategories)
+                            {
+                                SubCategory subCat = new SubCategory()
+                                {
+                                    Name = subCategory,
+                                    Link = baseLink + link.FirstChild.Attributes[0].Value,
+                                    PictureLink = baseLink + link.Attributes[0].Value,
+                                };
+                                category.SubCategories.Add(subCat);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return categories;
+        }
+
+        private Dictionary<string, string> ParseHtmlCategory(string html, string baseLink)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var node = htmlDoc.DocumentNode.SelectNodes("//li/a");
+
+            Dictionary<string, string> wikiLink = new Dictionary<string, string>();
+            Regex r = new Regex(@"\w.*\w", RegexOptions.Multiline);
+
+            List<Category> categories = new List<Category>();
+            if (node != null)
+            {
+                foreach (var link in node)
+                {
+                    Category category = categories.LastOrDefault();
+                    string catName = link.ParentNode.ParentNode.PreviousSibling.PreviousSibling.InnerText;
+
+                    if (catName != category?.Name)
+                    {
+                        category = new Category(catName);
+                        categories.Add(category);
+                    }
+                    if (link.Attributes.Count > 0)
+                    {
+                        string[] subcategories = r.Matches(link.InnerText)
+                                                .Cast<Match>()
+                                                .Select(m => m.Value)
+                                                .ToArray();
+
+                        if (subcategories != null)
+                        {
+                            SubCategory subCategory = new SubCategory()
+                            {
+                                Name = String.Join(',', subcategories),
+                                Link = baseLink + '/' + link.FirstChild.Attributes[0].Value,
+                                PictureLink = baseLink + '/' + link.Attributes[0].Value,
+                            };
+                            category.SubCategories.Add(subCategory);
+                        }
+                    }
+                }
+                foreach (Category category in categories)
+                {
+                    WriteToCsv(category);
+                }
+            }
+
+            return wikiLink;
+        }
+
+        private async Task<string> CallUrl(string fullUrl)
+        {
+            HttpClient client = new HttpClient();
+            var response = client.GetStringAsync(fullUrl);
+            return await response;
+        }
+
+        private void WriteToCsv(Category category, string fileName = null)
+        {
+            String catategoryCompany = String.Join(
+                Environment.NewLine,
+                category.SubCategories.Select(d => $"{d.Name};{d.Link};{d.PictureLink}")
+            );
+
+            File.AppendAllText(Path.Combine(projPath, "data", fileName??category.Name + ".csv"), catategoryCompany);
+        }
+    }
+}
---
+++ WebScraper/Scraper.cs
@@ -0,0 +1,68 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+
+namespace WebScraper
+{
+    class Scraper
+    {
+        static string projPath = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+        static void Main(string[] args)
+        {
+            Parser parser = new Parser();
+            //ClearCSVFiles();
+            var companies = parser.GetCompanies("https://audio-database.com", "links");
+        }
+
+        static void ClearCSVFiles()
+        {
+            var files = Directory.GetFiles(Path.Combine(projPath, "data"), "*.csv").ToList();
+            files.ForEach(file => File.WriteAllText(file, string.Empty));
+        }
+    }
+
+    class Company
+    {
+        internal string Name { get; set; }
+        internal string Link { get; set; }
+        internal List<Category> Categories = new List<Category>();
+    }
+
+    class Category
+    {
+        internal string Name { get; set; }
+        internal List<SubCategory> SubCategories = new List<SubCategory>();
+
+        public Category(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class SubCategory
+    {
+        internal string Name { get; set; }
+        internal string Link { get; set; }
+        internal string PictureLink { get; set; }
+
+        public SubCategory()
+        {
+        }
+    }
+
+    class Product
+    {
+        internal string Name { get; set; }
+        internal string Link { get; set; }
+        internal string PictureLink { get; set; }
+    }
+}
