c9bc1dee756c45baffc480531b47d89e15ee4003
Benek07 <wojciechbednarz96@gmail.com>
2023-03-12 10:50:22 +0100

    Configure layout and home page Add fontawesome to css

---
+++ AudioArea.Mvc/Components/CategoryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Packt.Shared;
+
+namespace AudioArea.Mvc.Components
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        protected readonly AudioContext db;
+
+        public CategoryViewComponent(AudioContext injectedContext)
+        {
+            db = injectedContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            IList<Category> Categories = await db.Categories.Take(5).ToListAsync();
+            return View(Categories);
+        }
+    }
+}
