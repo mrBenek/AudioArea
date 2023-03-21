b8a103642e57f96fd9a2846c8ba75388f084f3d6
Benek07 <wojciechbednarz96@gmail.com>
2023-03-21 18:40:22 +0100

    Design website - Add checkboxes to filter window with rotate arrow

--- DesignWebsite/js/site.js
+++ DesignWebsite/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(".btn-rotate").click(function () {
+    $(this).find(".chevron-rotate").toggleClass("down");
+})
