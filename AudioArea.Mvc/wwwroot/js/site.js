dcdb18201263db05a3dfe027efe29bd94ac25b23
wojciech-be <wojtekok1996@o2.pl>
2023-06-03 16:53:04 +0200

    Apply changes from design website: rotate arrow, images settings, button "Apply filters"

--- AudioArea.Mvc/wwwroot/js/site.js
+++ AudioArea.Mvc/wwwroot/js/site.js
@@ -1,4 +1,31 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var btnApplyFilters = document.getElementById('btn-apply-filters');
+var categoryCollapse = document.getElementById('category-collapse');
 
-// Write your JavaScript code.
+function changeWidthApplyFilters() {
+    btnApplyFilters.style.width = `${$(".col-xl-3:first-child").width()}px`;
+}
+
+window.onload = function () {
+    changeStyleApplyFiltersBtn();
+    changeWidthApplyFilters();
+};
+
+$(".btn-rotate").click(function () {
+    $(this).find(".chevron-rotate").toggleClass("down");
+})
+
+function changeStyleApplyFiltersBtn() {
+    var x = categoryCollapse.getBoundingClientRect().bottom;
+    //        console.log((window.innerHeight || document.documentElement.clientHeight));
+    if (x <= (window.innerHeight || document.documentElement.clientHeight))
+        btnApplyFilters.classList.remove("flow");
+    if (btnApplyFilters.getBoundingClientRect().bottom > (window.innerHeight || document.documentElement.clientHeight))
+        btnApplyFilters.classList.add("flow");
+}
+
+categoryCollapse.addEventListener('hidden.bs.collapse', () => changeStyleApplyFiltersBtn())
+categoryCollapse.addEventListener('shown.bs.collapse', () => changeStyleApplyFiltersBtn())
+document.addEventListener('scroll', () => changeStyleApplyFiltersBtn(), {
+    passive: true
+});
+addEventListener("resize", (event) => changeWidthApplyFilters());
