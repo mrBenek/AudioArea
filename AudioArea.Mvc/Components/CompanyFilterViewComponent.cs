ebffae4a9162d9bfb73e7eb0daeae80a3c8ac59a
wojciech-be <wojtekok1996@o2.pl>
2023-06-14 13:17:13 +0200

    Add component to show company filter

---
+++ AudioArea.Mvc/Components/CompanyFilterViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Packt.Shared;
+
+namespace AudioArea.Mvc.Components
+{
+    public class CompanyFilterViewComponent : ViewComponent
+    {
+        private readonly AudioContext db;
+
+        public CompanyFilterViewComponent(AudioContext injectedContext)
+        {
+            db = injectedContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            IList<Company> companies = await db.Companies.ToListAsync();
+            return View(companies);
+        }
+    }
+}
