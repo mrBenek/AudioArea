97a01a53ab4bbade038bc09e109b79bb78b1f6d5
wojciech-be <wojtekok1996@o2.pl>
2023-08-30 10:31:35 +0200

    Page navigation move to components

---
+++ AudioArea.Mvc/Components/PageNavigationViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Packt.Shared;
+
+namespace AudioArea.Mvc.Components
+{
+    public class PageNavigationViewComponent : ViewComponent
+    {
+        private readonly AudioContext db;
+
+        public PageNavigationViewComponent(AudioContext injectedContext)
+        {
+            db = injectedContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
